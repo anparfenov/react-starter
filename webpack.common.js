42aa48619dd122fba968bfbaefb7a8e4354d6477
Andrey Parfenov <asleppiano@outlook.com>
2022-06-05 22:27:38 +0300

    adds copy plugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/index.tsx',
@@ -29,7 +30,12 @@ module.exports = {
     resolve: {
         extensions: ['.tsx', '.ts', '.js'],
     },
-    plugins: [new HtmlWebpackPlugin({ template: './public/index.html' })],
+    plugins: [
+        new HtmlWebpackPlugin({ template: './public/index.html' }),
+        new CopyWebpackPlugin({
+            patterns: ['./public/assets', './dist/assets']
+        })
+    ],
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
