f8692a0339474841435eee0bc90e2f684dc8cf9b
Andrey Parfenov <asleeppiano@outlook.com>
2022-10-06 23:12:26 +0300

    removes babel, fixes copy plugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,50 +1,40 @@
-const path = require('path');
-const fs = require('fs');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const fs = require("fs");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
-const plugins = [
-    new HtmlWebpackPlugin({ template: './public/index.html' }),
-];
-if (fs.existsSync(path.resolve(__dirname, 'public/assets'))) {
-    plugins.push(
-	new CopyWebpackPlugin({
-            patterns: ['./public/assets', './dist/assets']
-	})
-    );
+const plugins = [new HtmlWebpackPlugin({ template: "./public/index.html" })];
+if (fs.existsSync(path.resolve(__dirname, "public/assets"))) {
+  plugins.push(
+    new CopyWebpackPlugin({
+        patterns: [
+            { from: "./public/assets", to: "assets" },
+          ],
+    })
+  );
 }
 
 module.exports = {
-    entry: './src/index.tsx',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.ts$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                }
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    { loader: 'css-loader' },
-                ],
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    plugins,
+  entry: "./src/index.tsx",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", { loader: "css-loader" }],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins,
 };
